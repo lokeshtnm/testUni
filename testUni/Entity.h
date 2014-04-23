597ae8f35420b5041807cfe7ac2a5d2dc0ba8301
Lokesh Tnm <lokeshtnm@gmail.com>
2014-04-23 18:39:08 +0530

    sss

---
+++ testUni/Entity.h
@@ -0,0 +1,18 @@
+//
+//  Entity.h
+//  testUni
+//
+//  Created by TNM_ios2 on 23/04/14.
+//  Copyright (c) 2014 TNM_ios2. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import <CoreData/CoreData.h>
+
+
+@interface Entity : NSManagedObject
+
+@property (nonatomic, retain) NSString * mobileno;
+@property (nonatomic, retain) NSString * name;
+
+@end
---
+++ testUni/Entity.m
@@ -0,0 +1,17 @@
+//
+//  Entity.m
+//  testUni
+//
+//  Created by TNM_ios2 on 23/04/14.
+//  Copyright (c) 2014 TNM_ios2. All rights reserved.
+//
+
+#import "Entity.h"
+
+
+@implementation Entity
+
+@dynamic mobileno;
+@dynamic name;
+
+@end
